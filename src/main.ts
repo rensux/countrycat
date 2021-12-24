0588885f89868d6c35af99279d085078743f4b3c
lmyaoi <renzo.russel@gmail.com>
2021-12-24 04:16:35 +0100

    commented out generator line

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { stringify } from "csv/sync";
 import { readFileSync, writeFileSync } from "fs";
 import { parseCsv } from "./util/read-data";
+import { genSeasonFileMap } from "./util/season-map";
 import { genSeasons } from "./util/seasons";
 import { aggregateCountries, rowify } from "./util/structure-data";
 import { transpose } from "./util/transpose";
 import { Map } from "./util/types";
 
 const seasons = genSeasons(18, (4*4) + 1)
+// genSeasonFileMap(seasons) // generate a mapping from season to data file
 const seasonFileMap = JSON.parse(readFileSync("out/season-map.json").toString())
 
 const countryMap: Map = {}
